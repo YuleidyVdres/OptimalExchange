ef70d2730b132c1773df71a9060f1808692c132b
Yuleidy Valladares <yuleidyvalladares@gmail.com>
2018-09-19 10:29:04 -0400

    :octocat: Initial commit

--- Currency/src/currency/Currency.java
+++ Currency/src/currency/Currency.java
@@ -3,13 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package monedas;
+package currency;
 
 /**
  *
  * @author yuleidyvdres
  */
-public class Monedas {
+public class Currency {
 
     /**
      * @param args the command line arguments
