a7e81c49f66bb12460d383834a5a329ceef77e84
Yuleidy Valladares <yuleidyvalladares@gmail.com>
2018-09-19 22:33:24 -0400

    :octocat: Base case

--- Currency/src/currency/Currency.java
+++ Currency/src/currency/Currency.java
@@ -1,21 +1,95 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package currency;
 
-/**
- *
- * @author yuleidyvdres
- */
-public class Currency {
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-    /**
-     * @param args the command line arguments
-     */
+public class Currency {
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        if(args.length == 1){
+            try {
+                BufferedReader input = new BufferedReader(new FileReader(args[0]));
+                int cases = Integer.parseInt(input.readLine());
+                String line;
+                int amount, cont, acum = 0, maximun = -1;
+                float prom;
+                for (int i = 0; i < cases; i++) {
+                    cont = 0;
+                    line = input.readLine();
+                    String [] aux = line.split(" ");
+                    /*Save amount per line*/
+                    amount = Integer.parseInt(aux[0]);
+                    /*Size for denomination array coins per line*/
+                    int [] denomination = new int[Integer.parseInt(aux[1])];
+                    for (int j = 2; j < aux.length; j++) {
+                        /*Save the corresponding denomination coin*/
+                        denomination[cont] = Integer.parseInt(aux[j]);
+                        cont++;
+                    }
+                    int [] minimun = min_coins(denomination, amount);
+                    for (int j = 1; j < minimun.length; j++) {
+                        acum+=minimun[j];
+                        System.out.println("Min "+ j +" " + minimun[j]);
+                    }
+                    prom = (float)acum/(float)amount;
+                    maximun = max(minimun);
+                    System.out.println(prom + " " + maximun);
+                    System.out.println("\n");
+                }
+                
+            } catch (FileNotFoundException ex) {
+                Logger.getLogger(Currency.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (IOException ex) {
+                Logger.getLogger(Currency.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        else{
+            System.out.println("You should specify an input file");
+        }
+    }
+    
+    public static int [] min_coins(int [] denomination, int amount){
+        int [] coins = new int[amount + 1];      
+        int aux;
+        /*The first element is filled with 0 and the rest with oo*/
+        for (int i = 0; i <= amount; i++) {
+            if(i == 0)
+                coins[i] = 0;
+            else
+                coins[i] = Integer.MAX_VALUE;
+        }
+        /*For each number in the range [1-N] compare all the denomination coins */
+        for (int i = 1; i <= amount; i++) {
+            System.out.println("\n");
+            for (int j = 0; j < denomination.length; j++) {
+                //System.out.println(denomination[j] +" <= "+i);
+                if(denomination[j] <= i){
+                    aux = coins[i - denomination[j]];
+                    //System.out.println("aux + 1 "+ aux+" < "+coins[i]);
+                    if(aux != Integer.MAX_VALUE && aux + 1 < coins[i]){
+                        //Save the actual min
+                        coins[i] = aux + 1;
+                    }
+                }
+            }
+        }
+        
+        return coins;
+    }
+    
+    public static int max(int [] array){
+        int max_number = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if(array[i] > max_number){
+                max_number = array[i];
+            }
+        }
+        
+        return max_number;
     }
     
 }
